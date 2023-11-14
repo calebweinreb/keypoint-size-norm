bugfix - use morph inverse jacobian in loss function

kpsn/fitting/em.py
@@ -70,20 +70,27 @@ def _mstep_objective(
 
     params = query_params.with_hyperparams(hyperparams)
 
-    poses = model.morph.inverse_transform(
-        params.morph, observations.keypts, observations.subject_ids)
+    poses, jacobian_logdets = model.morph.inverse_transform(
+        params.morph, observations.keypts, observations.subject_ids,
+        return_determinants = True)
     pose_probs = model.posespace.pose_logprob(
         params.posespace, poses, observations.subject_ids)
-    dataset_prob = (pose_probs * aux_pdf).sum()
+    jacobian_logdets = jacobian_logdets[..., None] # (Nt, L)
+    keypt_probs = pose_probs + jacobian_logdets
+    
+    dataset_prob = (keypt_probs * aux_pdf).sum()
     
     morph_prior = model.morph.log_prior(params.morph)
     posespace_prior = model.posespace.log_prior(params.posespace)
     
     # return morph_prior
     return {
-        'dataset': dataset_prob,
-        'morph': morph_prior,
-        'pose': posespace_prior}
+        'objectives': {
+            'dataset': dataset_prob,
+            'morph': morph_prior,
+            'pose': posespace_prior},
+        'aux': {'logjac': jacobian_logdets.mean()}
+    }
 
 
 def _mstep_loss(
@@ -95,9 +102,10 @@ def _mstep_loss(
         hyperparams: joint_model.JointParameters,
         aux_pdf: Float[Array, "Nt L"], 
         ):
-        objectives = _mstep_objective(
+        scalars = _mstep_objective(
             model, emissions, params, hyperparams, aux_pdf)
-        loss = -_pytree_sum(objectives)
+        loss = -_pytree_sum(scalars['objectives'])
+        objectives = {**scalars["objectives"], **scalars['aux']}
         return loss, objectives
     return step_loss_
 
@@ -127,7 +135,6 @@ def construct_jitted_mstep(
                 grads)
         
         if update_blacklist is not None:
-            # jax.debug.print("before {}", grads)
             grads = pt.tree_map_with_path(
                 lambda pth, grad: (
                     jnp.zeros_like(grad)
@@ -135,7 +142,6 @@ def construct_jitted_mstep(
                            for p in update_blacklist)
                     else grad),
                 grads)
-            # jax.debug.print("after {}", grads)
         
         updates, opt_state = optimizer.update(grads, opt_state, params)
         params = optax.apply_updates(params, updates)

kpsn/models/morph/affine_mode.py
@@ -107,8 +107,9 @@ class AFMParameters(NamedTuple):
     
     @staticmethod
     def normalize_offset_updates(offset_updates, identity_sess):
-        return offset_updates
-        # return offset_updates.at[identity_sess].set(0)
+        if identity_sess is None:
+            return offset_updates
+        return offset_updates.at[identity_sess].set(0)
     
     @property
     def mode_updates(self):
@@ -118,8 +119,9 @@ class AFMParameters(NamedTuple):
     
     @staticmethod
     def normalize_mode_updates(mode_updates, identity_sess):
-        return mode_updates
-        # return mode_updates.at[identity_sess].set(0)
+        if identity_sess is None:
+            return mode_updates
+        return mode_updates.at[identity_sess].set(0)
     
 
 
@@ -180,15 +182,16 @@ def transform(
 def inverse_transform(
     params: AFMParameters,
     keypts: Float[Array, "*#K M"],
-    sess_ids: Integer[Array, "*#K"]
-    ) -> Float[Array, "*#K M"]:
+    sess_ids: Integer[Array, "*#K"],
+    return_determinants: bool = False
+    ) -> Tuple[Float[Array, "*#K M"], Float[Array, "*#K"]]:
 
     linear_parts, pop_offset, sess_offsets = get_transform(params)
-    linear_inv = jla.inv(linear_parts)
+    linear_invs = jla.inv(linear_parts)
 
     # broadcast transform arrays
     batch_shape = sess_ids.shape
-    linear_inv = linear_inv[sess_ids] # (batch, M, M)
+    linear_inv = linear_invs[sess_ids] # (batch, M, M)
     pop_offset = broadcast_batch(pop_offset, batch_shape) # (batch, M)
     sess_offsets = sess_offsets[sess_ids] # (batch, M)
 
@@ -196,8 +199,13 @@ def inverse_transform(
     centered = (keypts - sess_offsets)[..., None] # (batch, M, 1)
     updated = (linear_inv @ centered)[..., 0] # (batch, M)
     uncentered = updated + pop_offset # (batch, M)
-
-    return uncentered
+    
+    if return_determinants:
+        linear_invs_logdet = jnp.log(jla.det(linear_invs))
+        # jax.debug.print("linvsh {}", linear_invs_det)
+        linear_inv_logdet = linear_invs_logdet[sess_ids]
+        return uncentered, linear_inv_logdet
+    else: return uncentered
     
 
 

test/kpsn_test/visualize/mixtures.py
@@ -29,6 +29,8 @@ def gaussian_mle(
     means = np.empty([len(x), M])
     covs = np.empty([len(x), M, M])
     for i in range(len(x)):
+        if len(x[i]) < 2:
+            means[i] = covs[i] = None; continue
         gm = mixture.GaussianMixture(n_components = 1,).fit(x[i])
         means[i] = gm.means_
         covs[i] = gm.covariances_
@@ -117,8 +119,12 @@ def combine_vrng(*vrngs):
     Returns:
         :param vrng: Tuple of `xrng`, `yrng`"""
     vmins, vmaxs = zip(*(np.array(vrng).T for vrng in vrngs))
-    vmin = np.minimum(*vmins)
-    vmax = np.maximum(*vmaxs)
+    vmins = np.stack(vmins, axis = 0)
+    vmaxs = np.stack(vmaxs, axis = 0)
+    vmins[~np.isfinite(vmins)] = np.nan
+    vmaxs[~np.isfinite(vmaxs)] = np.nan
+    vmin = np.nanmin(vmins, axis = 0)
+    vmax = np.nanmax(vmaxs, axis = 0)
     return np.array([vmin, vmax]).T
 
 def apply_vrng(ax, vrng):
@@ -148,16 +154,17 @@ def plot_cov_ellipses_comparison(
     **artist_kwargs
     ):
 
-    vrng1 = plot_many_fading_cov_ellipses(
+    vrng = plot_many_fading_cov_ellipses(
         x1, A1,
         pal = np.zeros([len(x1), 3]), ax = ax,
         **{'lw': 0.5, **artist_kwargs})
-    vrng2 = plot_many_fading_cov_ellipses(
-        x2, A2,
-        pal = pal,
-        ax = ax,
-        **{**artist_kwargs, 'fill': True, 'lw': 0})
-    vrng = combine_vrng(vrng1, vrng2)
+    if x2 is not None:
+        vrng2 = plot_many_fading_cov_ellipses(
+            x2, A2,
+            pal = pal,
+            ax = ax,
+            **{**artist_kwargs, 'fill': True, 'lw': 0})
+        vrng = combine_vrng(vrng, vrng2)
 
     return vrng
         