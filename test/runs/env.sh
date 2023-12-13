db10de4c04a25dc57120da8adb274d0d001235e5
Kai Fox <kai_fox@hms.harvard.edu>
2023-12-13 12:36:00 -0500

    remove run scripts

--- test/runs/env.sh
+++
@@ -1,7 +0,0 @@
-source $HOME/bin/activate/mph
-
-export PROJ=$HOME/projects/mph/generative
-export RESULT=$PROJ/results
-export PLOT=$PROJ/plots
-export SCRIPT=$PROJ/test/scripts
-export RTN=$PROJ/test/kpsn_test/routines
